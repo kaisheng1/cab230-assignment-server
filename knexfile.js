d268a88fdee75e4d850cf98987c88a3bf9ca760d
kaisheng1 <tks.2567@gmail.com>
2020-02-10 00:41:32 +1000

    Revert "try revert"

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,10 @@
+console.log(process.env.DB_HOST);
+
 module.exports = {
 	client: 'mysql',
 	connection: {
 		host: process.env.DB_HOST,
-		database: process.env.DB_DATABASE,
+		database: 'web_computing',
 		user: process.env.DB_USER,
 		password: process.env.DB_PASSWORD
 	}
